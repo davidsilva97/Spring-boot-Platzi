2ab78059609e66bf310b7605c98968da9cad2af7
David Silva <96149854+davidsilva97@users.noreply.github.com>
2022-03-19 13:30:50 -0600

    .

---
+++ src/main/java/com/fundamentosplatzi/FundamentosplatziApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosplatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosplatziApplication.class, args);
+	}
+
+}
